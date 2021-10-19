cdd1927e0ce866a96ffe9a519057ff42089f89ad
WillLipscomb <90565417+WillLipscomb@users.noreply.github.com>
2021-10-18 22:04:52 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -6,12 +6,13 @@ let elem = [];
 elem[1] = document.getElementById("1");
 
 //Assign a function to the + button
-elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
+//elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
 
-//Assign functions to remaining + buttons
+//Assign functions to all buttons
 for(let i=1; i<=18; i++) {
   elem[i] = document.getElementById(i.toString());
   elem[i].children[4].children[0].onclick = function(){add1(elem[i]);};
+  elem[i].children[4].children[1].onclick = function(){sub1(elem[i]);};
 }
 
 //Create an "add1" function
@@ -24,3 +25,15 @@ function add1 (elem) {
     elem.children[2].innerHTML = currentScore + 1;
   }
 }
+
+//Create subtraction function
+function sub1 (elem){
+  if(elem.children[2].innerHTML == "1"){
+    elem.children[2].innterHTML = "-";
+  } else {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore - 1;
+  }
+}
+
