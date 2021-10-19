436fcb72719be4056d80a74614f360d8f0845c57
WillLipscomb <90565417+WillLipscomb@users.noreply.github.com>
2021-10-18 22:01:07 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -14,7 +14,7 @@ for(let i=1; i<=18; i++) {
   elem[i].children[4].children[0].onclick = function(){add1(elem[i]);};
 }
 
-// create an "add1" function
+//Create an "add1" function
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") {
     elem.children[2].innerHTML = "1";
