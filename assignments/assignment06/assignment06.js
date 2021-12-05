d8521e2cc6e6df70b38aee4a1f9387b17a6bf910
WillLipscomb <90565417+WillLipscomb@users.noreply.github.com>
2021-12-05 14:58:17 -0500

    Update assignment06.js

--- assignments/assignment06/assignment06.js
+++ assignments/assignment06/assignment06.js
@@ -106,5 +106,9 @@ function updateLoansArray() {
     //Update interest rates
     $("#loan_int0"+ (i+1) ).val(loans[i].loan_int_rate);
     loans[i].loan_int_rate = loans[0].loan_int_rate;
+    
+    //Update YE Bal
+    var loanWithInterest = loans[i].loan_amount * (1 + loans[i].loan_int_rate);
+    $("#loan_bal0" + (i+1)).html = toComma(loanWithInterest.toFixed(2));
   }
 }
